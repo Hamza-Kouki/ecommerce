75f267f0196f8b3ee1e0ae69f4dd8cca98e0e136
HamzaKouki <hamzakouki33@gmail.com>
2020-01-17 16:45:30 +0100

    newChangeAllProductsPage

--- details.php
+++ details.php
@@ -21,11 +21,11 @@
          <!-- menubar -->
             <div class="menubar">
               <ul id="menu">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">ALL Products</a></li>
-                  <li><a href="#">My Account</a></li>
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="all_products.php">ALL Products</a></li>
+                  <li><a href="customer/my_account.php">My Account</a></li>
                   <li><a href="#">Sign Up</a></li>
-                  <li><a href="#">Shopping Cart</a></li>
+                  <li><a href="cart.php">Shopping Cart</a></li>
                   <li><a href="#">Contact Us</a></li>
               </ul>
               <div id="form" action="get" action="results.php" enctype= multipart/form-data>
--- functions/functions.php
+++ functions/functions.php
@@ -176,16 +176,15 @@ function getBrandPro($page){
   }
 //================================================================================================================
 function getPro_all_products(){
-    global $con ; 
-if(!isset($_GET['search'])){
-    $search_query = $_GET['user_query'];
-    $get_pro =  "select *  from products where product_keywords like '%$search_query%'";
+    if((!isset($_GET['cat']))&&(!isset($_GET['brand']))){
+    global $con ;
+    $get_pro =  "select *  from products ";
 
     $run_pro = mysqli_query($con, $get_pro);
 
     while($row_pro = mysqli_fetch_array($run_pro)){
-        $pro_id    = $row_pro['product_id'];
-        $pro_cat   = $row_pro['product_cat'];
+        $pro_id = $row_pro['product_id'];
+        $pro_cat = $row_pro['product_cat'];
         $pro_brand = $row_pro['product_brand'];
         $pro_title = $row_pro['product_title'];
         $pro_price = $row_pro['product_price'];
@@ -201,7 +200,8 @@ if(!isset($_GET['search'])){
             <a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
         </div>
         ";
-    } }
+    }
+   }
   }
 // }
 //================================================================================================================
